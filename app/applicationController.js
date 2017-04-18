9f8364f6a626b24347ab619d5485503132510dfe
gowtham <gowtham@codingmart.com>
2017-04-18 20:39:04 +0530

    loading indicator

--- app/applicationController.js
+++ app/applicationController.js
@@ -1,14 +1,24 @@
 angular.module('app').controller('applicationController', function ($scope,$http) {
     
     $scope.movies = [];
-    $scope.users = ['Fabio', 'Leonardo', 'Thomas', 'Gabriele', 'Fabrizio', 'John', 'Luis', 'Kate', 'Max'];
+    $scope.movie = [];
+    $scope.myLoadingScope = false;
+    // $scope.viewFlag = false;
     $scope.init = function() {
+    	$scope.myLoadingScope = true;
 		$http.get('https://yts.ag/api/v2/list_movies.json?limit=15')
 		.then(function(response) {
-		  $scope.movies = response.data.data.movies;
-		  // console.log($scope.movies)
+		  	$scope.movies = response.data.data.movies;
+		  	$scope.myLoadingScope = false;
+		  	// console.log($scope.movies)
 		});
 	}
 
     $scope.init();
+
+    $scope.viewMovie = function(index) {
+    	// $scope.viewFlag = true;
+    	$scope.movie = angular.copy($scope.movies[index]);
+    	// console.log($scope.movie)
+    }
 });
