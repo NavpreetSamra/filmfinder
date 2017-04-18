21ec42499e4c7b682bf74cc1c946dd814f17b333
gowtham <gowtham@codingmart.com>
2017-04-18 22:02:07 +0530

    fixed header, hover effect, api calls

--- app/applicationController.js
+++ app/applicationController.js
@@ -3,22 +3,27 @@ angular.module('app').controller('applicationController', function ($scope,$http
     $scope.movies = [];
     $scope.movie = [];
     $scope.myLoadingScope = false;
-    // $scope.viewFlag = false;
-    $scope.init = function() {
+    $scope.getMovies = function(type) {
     	$scope.myLoadingScope = true;
-		$http.get('https://yts.ag/api/v2/list_movies.json?limit=15')
-		.then(function(response) {
-		  	$scope.movies = response.data.data.movies;
-		  	$scope.myLoadingScope = false;
-		  	// console.log($scope.movies)
-		});
+		url='';
+		$scope.movies = [];
+		if(type === 'Popular') {
+			url='https://yts.ag/api/v2/list_movies.json?limit=15&sort_by=download_count'
+		} else {
+			url='https://yts.ag/api/v2/list_movies.json?limit=15'
+		}
+		if(url != '') {
+			$http.get(url)
+			.then(function(response) {
+				$scope.movies = angular.copy(response.data.data.movies);
+				$scope.myLoadingScope = false;
+			});
+		}
 	}
 
-    $scope.init();
+    $scope.getMovies('Popular');
 
     $scope.viewMovie = function(index) {
-    	// $scope.viewFlag = true;
     	$scope.movie = angular.copy($scope.movies[index]);
-    	// console.log($scope.movie)
     }
 });
