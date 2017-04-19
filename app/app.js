35de0e050170098a6d9ae792264b7a28e7422094
gowtham <gowtham@codingmart.com>
2017-04-19 08:44:08 +0530

    fallback for no internet

--- app/app.js
+++ app/app.js
@@ -1,1 +1,5 @@
 angular.module('app', ['ngMaterial','ngLoadingScreen'])
+// .config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('default')
+//     .dark();
+// });
--- app/applicationController.js
+++ app/applicationController.js
@@ -1,27 +1,33 @@
-angular.module('app').controller('applicationController', function ($scope,$http) {
+angular.module('app').controller('applicationController', function ($scope,$http,$mdToast) {
     
     $scope.movies = [];
     $scope.movie = [];
     $scope.myLoadingScope = false;
     $scope.getMovies = function(type) {
-    	$scope.myLoadingScope = true;
 		url='';
-		$scope.movies = [];
 		if(type === 'Popular') {
 			url='https://yts.ag/api/v2/list_movies.json?limit=15&sort_by=download_count'
 		} else {
 			url='https://yts.ag/api/v2/list_movies.json?limit=15'
 		}
-		if(url != '') {
+		$scope.movies = [];
+		if(navigator.onLine) {
+			$scope.myLoadingScope = true;
 			$http.get(url)
 			.then(function(response) {
-				$scope.movies = angular.copy(response.data.data.movies);
 				$scope.myLoadingScope = false;
+				$scope.movies = angular.copy(response.data.data.movies);
 			});
 		}
+		else {
+			$mdToast.show(
+				$mdToast.simple()
+						.textContent('No Internet Connection')
+						.position('top, right')
+						.hideDelay(2000)
+			);
+		}
 	}
-
-    $scope.getMovies('Popular');
 
     $scope.viewMovie = function(index) {
     	$scope.movie = angular.copy($scope.movies[index]);
